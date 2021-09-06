540723e583ba576562a584b900f3ed14d06ee2c5
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-06 22:40:49 +0500

    input and button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 // libraries
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 
@@ -14,13 +14,42 @@ const BookmarkQuery = gql`
 `;
 
 export default function Home() {
+  const [inputUrl, setInputUrl] = useState("");
+  const [inputDesc, setInputDesc] = useState("");
+
   const { loading, error, data } = useQuery(BookmarkQuery);
 
-  console.log(data);
+  const addBookmarkSubmit = () => {
+    console.log(inputUrl);
+  };
 
   return (
     <div>
       <p>{JSON.stringify(data)}</p>
+
+      <div>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter URL"
+            value={inputUrl}
+            onChange={(e) => setInputUrl(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Enter Description"
+            value={inputDesc}
+            onChange={(e) => setInputDesc(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <button onClick={addBookmarkSubmit}>Add Bookmark</button>
+        </div>
+      </div>
     </div>
   );
 }
