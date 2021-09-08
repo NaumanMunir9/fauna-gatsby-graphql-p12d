3849c3dcf002cc8e85a465281a5e2e4746346b0a
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-08 15:43:11 +0500

    v3:43

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    // {
-    //   resolve: "gatsby-plugin-apollo",
-    //   options: {
-    //     uri: "https://mnm-p12d.netlify.app/",
-    //   },
-    // },
+    {
+      resolve: "gatsby-plugin-apollo",
+      options: {
+        uri: "https://mnm-p12d.netlify.app/.netlify/functions/bookmarks",
+      },
+    },
     `gatsby-plugin-material-ui`,
   ],
 };
